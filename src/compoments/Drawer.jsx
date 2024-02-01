5e792fc3ccd987dc0e18dc1e9ac75b18ea5dd9ce
Mayinuddin <mayinuddin.storerepublic@gmail.com>
2024-02-01 17:43:47 +0600

    dashboard almost done

--- src/compoments/Drawer.jsx
+++ src/compoments/Drawer.jsx
@@ -1,7 +1,9 @@
 // src/components/Drawer.js
 
-import React, { useState } from 'react';
-import './Drawer.scss';
+import React, { useState } from "react";
+import "./Drawer.scss";
+import close from "../assets/images/close.png";
+import { Link } from "react-router-dom";
 
 const Drawer = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(true);
@@ -11,11 +13,22 @@ const Drawer = () => {
   };
 
   return (
-    <div className={`drawer ${isDrawerOpen ? 'open' : ''}`}>
+    <div className={`drawer ${isDrawerOpen ? "open" : ""}`}>
       <button className="toggle-button" onClick={toggleDrawer}>
-        Toggle Drawer
+        <img src={close} alt="..." />
       </button>
       {/* Additional Drawer content goes here */}
+      <div>
+        <Link to="">
+          <p>content1</p>
+        </Link>
+        <Link to="">
+          <p>content2</p>
+        </Link>
+        <Link to="">
+          <p>content3</p>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Drawer from "../../compoments/Drawer";
+import Home from "../../pages/Home/Home";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-
   return (
     <div>
       {/* Your dashboard content goes here */}
+      <Outlet />
       <Drawer />
     </div>
   );
